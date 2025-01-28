02f399124b14513899587e2c519826b160de4c83
WhiteyCookie <102429233+WhiteyCookie@users.noreply.github.com>
2025-01-28 07:04:13 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  ...(process.env.NODE_ENV === 'production'
-    ? { basePath: '/bitaxe-web-flasher', assetPrefix: '/bitaxe-web-flasher' }
-    : {}),
   images: {
     unoptimized: true, // Optional: If you don't need image optimization
   },
